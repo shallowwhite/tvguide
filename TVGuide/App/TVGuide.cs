94e04424e3b654e57b35cd76cac90f898233f2b4
xsoliot <afearnehough+git@gmail.com>
2017-04-01 22:01:28 +0100

    Commit json version before xml

--- TVGuide/App/TVGuide.cs
+++ TVGuide/App/TVGuide.cs
@@ -11,21 +11,19 @@ namespace TVGuide.App
 {
     class TVGuide
     {
+        private string authenticationToken = "";
+
         public TVGuide()
         {
             // download tv listings for today
-            var data = getChannels();
+            var data = getAuthenticationToken();
         }
 
         public List<TVGuideChannel> getChannels()
         {
             List<TVGuideChannel> result = new List<TVGuideChannel>();
-            string apiUrl = String.Format(
-                "http://api.tvmaze.com/schedule?country=GB&date={0}",
-                DateTime.Now.ToString("yyyy-MM-dd"));
 
-
-            var programs = getJSON(apiUrl);
+            /*var programs = getJSON(apiUrl);
             string test = "";
             foreach (Dictionary<string, object> program in programs)
             {
@@ -35,19 +33,37 @@ namespace TVGuide.App
 
 
             }
-            MessageBox.Show(test);
+            MessageBox.Show(test);*/
             return result; 
         }
 
+        private string getTVData()
+        {
+            // download xmltv file
+            // convert to json
+
+        }
+
         /**
-         * Utility function to convert json from a http end point to a nested series of dictionaries
+         * requestJSON()
          */
-        private List<object> getJSON(string apiUrl)
+        private List<object> requestJSON(string url, string format = "application/json", string post = null)
         {
             try
             {
                 // fetch tv listings json
-                var request = WebRequest.Create(apiUrl) as HttpWebRequest;
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                request.ContentType = "application/json";
+                
+                if (jsonPost != null)
+                {
+                    request.Method = "POST";
+                    using (var writer = new StreamWriter(request.GetRequestStream()))
+                    {
+                        writer.Write(jsonPost);
+                    }
+                }
+
                 using (var response = request.GetResponse() as HttpWebResponse)
                 {
                     // check http request was successful
@@ -64,78 +80,87 @@ namespace TVGuide.App
                         using (var reader = new StreamReader(
                             response.GetResponseStream(), System.Text.Encoding.UTF8))
                         {
-                            // convert JSON object to a series of nested dictionaries
-
-                            Func<string, List<object>> parseJSONArr = null;
-                            Func<string, Dictionary<string, object>> parseJSONObj = null;
-
-                            parseJSONArr = (string json) =>
-                            {
-                                var values = JsonConvert.DeserializeObject<List<object>>(json);
-                                var result = new List<object>();
-
-                                foreach (object it in values)
-                                {
-                                    if (it is Newtonsoft.Json.Linq.JObject)
-                                    {
-                                        result.Add(parseJSONObj(it.ToString()));
-                                    }
-                                    else if (it is Newtonsoft.Json.Linq.JArray)
-                                    {
-                                        result.Add(parseJSONArr(it.ToString()));
-                                    }
-                                    else
-                                    {
-                                        result.Add(it);
-                                    }
-                                }
-
-                                return result;
-                            };
-                            parseJSONObj = (string json) =>
-                            {
-                                var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
-                                var result = new Dictionary<string, object>();
-
-                                foreach (KeyValuePair<string, object> it in values)
-                                {
-                                    if (it.Value is Newtonsoft.Json.Linq.JObject)
-                                    {
-                                        result.Add(it.Key, parseJSONObj(it.Value.ToString()));
-                                    }
-                                    else if (it.Value is Newtonsoft.Json.Linq.JArray)
-                                    {
-                                        result.Add(it.Key, parseJSONArr(it.Value.ToString()));
-                                    }
-                                    else
-                                    {
-                                        result.Add(it.Key, it.Value);
-                                    }
-                                }
-                                return result;
-                            };
-                            string responseText = reader.ReadToEnd().Trim();
-                            if (responseText[0] == '[')
-                            {
-                                // return list of objects
-                                return parseJSONArr(responseText);
-                            }
-                            else
-                            {
-                                // return list with single object
-                                List<object> result = new List<object>();
-                                result.Add(parseJSONObj(responseText));
-                                return result;
-                            }
+                            return convertJSON(reader.ReadToEnd());
                         }
                     }
                 }
             }
             catch (Exception e)
             {
-                MessageBox.Show(e.Message, "Could not get TV listings",
+                MessageBox.Show(e.Message, "Error",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return null;
+            }
+        }
+
+        /**
+         * convertJSON()
+         * Utility function to convert json from a url to nested series of
+         * Lists and Dictionaries
+         */
+        private List<Object> convertJSON(string json)
+        {
+            // convert JSON object to a series of nested dictionaries
+            Func<string, List<object>> parseJSONArr = null;
+            Func<string, Dictionary<string, object>> parseJSONObj = null;
+
+            parseJSONArr = (string jsonString) =>
+            {
+                var values = JsonConvert.DeserializeObject<List<object>>(jsonString);
+                var result = new List<object>();
+
+                foreach (object it in values)
+                {
+                    if (it is Newtonsoft.Json.Linq.JObject)
+                    {
+                        result.Add(parseJSONObj(it.ToString()));
+                    }
+                    else if (it is Newtonsoft.Json.Linq.JArray)
+                    {
+                        result.Add(parseJSONArr(it.ToString()));
+                    }
+                    else
+                    {
+                        result.Add(it);
+                    }
+                }
+
+                return result;
+            };
+            parseJSONObj = (string jsonString) =>
+            {
+                var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+                var result = new Dictionary<string, object>();
+
+                foreach (KeyValuePair<string, object> it in values)
+                {
+                    if (it.Value is Newtonsoft.Json.Linq.JObject)
+                    {
+                        result.Add(it.Key, parseJSONObj(it.Value.ToString()));
+                    }
+                    else if (it.Value is Newtonsoft.Json.Linq.JArray)
+                    {
+                        result.Add(it.Key, parseJSONArr(it.Value.ToString()));
+                    }
+                    else
+                    {
+                        result.Add(it.Key, it.Value);
+                    }
+                }
+                return result;
+            };
+            json = json.Trim();
+            if (json[0] == '[') // check if root element is an array
+            {
+                // return list of objects
+                return parseJSONArr(json);
+            }
+            else
+            {
+                // return list with single object
+                List<object> result = new List<object>();
+                result.Add(parseJSONObj(json));
+                return result;
             }
         }
     }
